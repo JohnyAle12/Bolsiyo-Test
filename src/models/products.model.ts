3ed5625c104721e711515b6aa8d79a3d2882618e
Johny Prieto <johnyprieto001@gmail.com>
2022-12-01 20:44:30 -0500

    add datasource and products model, controller

---
+++ src/models/products.model.ts
@@ -0,0 +1,70 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Products extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  code: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  description: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  brand: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  category_id: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  quantity: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  price: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  company_id: string;
+
+
+  constructor(data?: Partial<Products>) {
+    super(data);
+  }
+}
+
+export interface ProductsRelations {
+  // describe navigational properties here
+}
+
+export type ProductsWithRelations = Products & ProductsRelations;
