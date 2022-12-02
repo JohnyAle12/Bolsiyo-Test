cd464b1bf3e7ab1be1fa75e9ef5d270eb24343b4
Johny Prieto <johnyprieto001@gmail.com>
2022-12-02 17:08:09 -0500

    delete company controller

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,4 +1,4 @@
+export * from './category.controller';
 export * from './ping.controller';
 export * from './products.controller';
-export * from './category.controller';
-export * from './company.controller';
+
